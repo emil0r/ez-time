a9b2d581d9dd5a718617a45ad01f16bbb625d143
Emil Bengtsson <emil@emil0r.com>
2014-05-21 15:05:38 +0200

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject ez-time "0.1.0-SNAPSHOT"
+  :description "Easy time manipulation"
+  :url "https://github.com/emil0r/ez-time"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
