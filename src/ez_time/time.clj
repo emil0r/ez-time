4cd92e9a4a7616d76844e7bf16fd7591071f347e
Emil Bengtsson <emil@emil0r.com>
2014-07-01 15:28:03 +0200

    convert between year, month and milliseconds

--- src/ez_time/time.clj
+++ src/ez_time/time.clj
@@ -8,34 +8,6 @@
                           YearMonth LocalDate)))
 
 
-(def ^:private months {"Jan" 1
-                       "Feb" 2
-                       "Mar" 3
-                       "Apr" 4
-                       "May" 5
-                       "Jun" 6
-                       "Jul" 7
-                       "Aug" 8
-                       "Sep" 9
-                       "Oct" 10
-                       "Nov" 11
-                       "Dec" 12})
-
-(defn- read-leapseconds []
-  (let [data (io/reader (io/resource "tzdata/leapseconds"))]
-    (map (fn [[_ year month day & _]]
-           {:year (Long/parseLong year)
-            :month (get months month)
-            :day (Long/parseLong day)
-            :leap-ms 1000 ;; leap second in milliseconds
-            })
-     (map #(str/split % #"\t")
-          (filter #(re-find #"^Leap" %)
-                  (line-seq data))))))
-
-(def ^:private leapseconds (read-leapseconds))
-
-
 (defprotocol EzTimeProtocol
   (year [obj])
   (month [obj])
@@ -120,19 +92,28 @@
 
 
 
-(defn year->ms
-  "Get back the year in number of milliseconds from or before 1970"
-  [year & [epoch?]]
-  ;; have 1970 as the origo because of epoch time
-  (let [year-range (if (>= year 1970) (range 1970 year) (range year 1970))
-        days (apply + (map (fn [y] (if (util/leap? y) 366 365)) year-range))]
-    (if (>= year 1970)
-      (if epoch?
-        (* days 86400000)
-        (* days 86400000)) ;; add check against leap seconds
-      (* days -86400000))))
-
 (defmulti datetime (fn [& args] (type (last args))))
+(defmethod datetime ez_time.timezone.TimeZone
+  ([year tz]
+     (datetime year 1 1 0 0 0 0 tz))
+  ([year month tz]
+     (datetime year month 1 0 0 0 0 tz))
+  ([year month day tz]
+     (datetime year month day 0 0 0 0 tz))
+  ([year month day hour tz]
+     (datetime year month day hour 0 0 0 tz))
+  ([year month day hour minute tz]
+     (datetime year month day hour minute 0 0 tz))
+  ([year month day hour minute second tz]
+     (datetime year month day hour minute second 0 tz))
+  ([year month day hour minute second millisecond tz]
+     (EzTime. (+ (util/year->ms year)
+                 (util/month->ms year month)
+                 (* day 24 3600 1000)
+                 (* hour 3600 1000)
+                 (* minute 60 1000)
+                 (* second 1000)
+                 millisecond) tz)))
 (defmethod datetime :default
   ([year]
      (datetime year 1 1 0 0 0 0))
@@ -146,6 +127,11 @@
      (datetime year month day hour minute 0 0))
   ([year month day hour minute second]
      (datetime year month day hour minute second 0))
-  ([year month day hour
-    minute second millis]
-     (EzTime. 0 tz/utc)))
+  ([year month day hour minute second millisecond]
+     (EzTime. (+ (util/year->ms year)
+                 (util/month->ms year month)
+                 (* day 24 3600 1000)
+                 (* hour 3600 1000)
+                 (* minute 60 1000)
+                 (* second 1000)
+                 millisecond) nil)))
--- src/ez_time/util.clj
+++ src/ez_time/util.clj
@@ -3,6 +3,104 @@
 
 (defn leap? [year]
   (and (= (mod year 4) 0) ;; dividable by 4
-       (or (not (= (mod year 100) 0)) ;; not a leap year if divideable by 100
+       (or (not= (mod year 100) 0) ;; not a leap year if divideable by 100
            (= (mod year 400) 0)) ;; unless it's also divideable by 400
        ))
+
+(def ms-per-day (* 24 60 60 1000))
+(def ms-per-year (* 365 24 60 60 1000))
+(def ms-per-leap-year (* 366 24 60 60 1000))
+
+
+
+(defn leap-days
+  "Number of leap days with 1970 as the origo"
+  [year]
+  (let [year (Math/abs year)]
+    (Math/abs
+     (- (+ (long (/ year 4)) (long (/ year 400)))
+        (long (/ year 100))
+        477))))
+
+(defn get-days [year]
+  (+ (Math/abs (* 365 (- year 1970)))
+     (leap-days year)))
+
+
+(defn year->ms
+  "Get back the year in number of milliseconds from or before 1970"
+  [year]
+  ;; have 1970 as the origo because of epoch time
+  (if (>= year 1970)
+    (* (get-days year) ms-per-day)
+    (* (get-days year) -1 ms-per-day)))
+
+(defn month->ms [year month]
+  (if (leap? year)
+    (case month
+      12 31622400000 
+      11 28944000000
+      10 26352000000
+      9 23673600000
+      8 21081600000
+      7 18403200000
+      6 15724800000
+      5 13132800000
+      4 10454400000
+      3 7862400000
+      2 5184000000
+      2678400000)
+    (case month
+      12 31536000000
+      11 28857600000
+      10 26265600000
+      9 23587200000
+      8 20995200000
+      7 18316800000
+      6 15638400000
+      5 13046400000
+      4 10368000000
+      3 7776000000
+      2 5097600000
+      2678400000)))
+
+(defn ms->year [ms]
+  (loop [year (+ 1970 (long (/ ms ms-per-year)))]
+    (let [year-ms (year->ms year)
+          diff (- ms year-ms)]
+      (cond
+       (< diff 0) (recur (- year 1))
+       (> diff ms-per-leap-year) (recur (+ year 1))
+       (> diff ms-per-year) (recur (+ year 1))
+       :else year))))
+
+(defn ms->month [ms]
+  (let [year (milliseconds->year ms)
+        ms (- ms (* (get-days year) ms-per-day))]
+    (if (leap? year)
+      (cond
+       (>= ms 31622400000) 12
+       (>= ms 28944000000) 11
+       (>= ms 26352000000) 10
+       (>= ms 23673600000) 9
+       (>= ms 21081600000) 8
+       (>= ms 18403200000) 7
+       (>= ms 15724800000) 6
+       (>= ms 13132800000) 5
+       (>= ms 10454400000) 4
+       (>= ms 7862400000) 3
+       (>= ms 5184000000) 2
+       :else 1)
+      (cond
+       (>= ms 31536000000) 12
+       (>= ms 28857600000) 11
+       (>= ms 26265600000) 10
+       (>= ms 23587200000) 9
+       (>= ms 20995200000) 8
+       (>= ms 18316800000) 7
+       (>= ms 15638400000) 6
+       (>= ms 13046400000) 5
+       (>= ms 10368000000) 4
+       (>= ms 7776000000) 3
+       (>= ms 5097600000) 2
+       :else 1))))
